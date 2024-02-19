b69e95567c93e6ee58c37342433d4e13b9aeb16b
Dmitriy <isakov.dmitriy@list.ru>
2024-02-19 14:20:36 +0300

    4 commit

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,6 @@ Route::get('/', function () {
 });
 
 Route::get('about', function () {
-    return view('about');
+    $tags = ['обучение', 'программирование', 'php', 'oop'];
+    return view('about', ['tags' => $tags]);
 });

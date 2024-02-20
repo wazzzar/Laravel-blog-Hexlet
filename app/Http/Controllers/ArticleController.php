745371a1080c29b01837ba63bd822f6b0f1e214e
Dmitriy <isakov.dmitriy@list.ru>
2024-02-20 13:30:15 +0300

    14 commit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
+
 class ArticleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $articles = Article::paginate();
-        return view('article.index', compact('articles'));
+        $q = '';
+        if ($request->has('q') && ($q = $request->get('q'))) {
+            $articles = Article::where('name', 'LIKE', "%{$q}%")->paginate(10);
+        } else {
+            $articles = Article::paginate(10);
+        }
+        return view('article.index', compact('articles', 'q'));
     }
 
     public function show($id){
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,6 +3,10 @@
 @section('header', 'Список статей')
 
 @section('content')
+    {{Form::open(['route' => 'articles.index', 'method' => 'GET'])}}
+        {{Form::text('q', $q)}}
+        {{Form::submit('Search')}}
+    {{Form::close()}}
     @foreach ($articles as $article)
         <h2><a href="{{route('articles.show', $article)}}">{{$article->name}}</a></h2>
         <div>{{Str::limit($article->body, 200)}}</div>
